56efb6c2e69ffa934ca3ef6bf63a2aea7c91ef42
Ajinkya Mukherjee <aam5802@outlook.com>
2020-12-25 23:20:45 +0000

    New Commit

--- ElGamal.py
+++ ElGamal.py
@@ -15,18 +15,20 @@ def HCF(num1,num2):
         return num2
     else:
         return HCF(num2,num1%num2)
+
+# Function to check if number is coPrime or not
 def coprime(num1,num2):
     return HCF(num1,num2)==1
 
+# Function to check if a given number is a natural number or not
 def natural(num):
     if num>0 and type(num) == int:
         return 1
     else:
         return 0
     
-
-
-def EulerGroup(x): # TO get a list of numbers in Z*q
+# Function to get a list of number in Z*q
+def EulerGroup(x):
     phiGroup=[]
     if x == 1:
         return 1
@@ -46,12 +48,10 @@ def primefactor(n):
     while(n%2)==0:
         factorlist.append(2)
         n=n/2
-    
     for i in range(3,int(math.sqrt(n))+1,2):
         while n%i==0:
             factorlist.append(i)
             n=n/i
-
     if n>2:
         factorlist.append(n)
 
@@ -92,10 +92,13 @@ def numInGroup(x): # function to define the elements in quadratic group
 # num1 * itr = 1 mod num2
 # Return Itr which is multiplicative Inverse of num1 while dividing with num2 resulting in 1 as reminader
 def multInverse(num1,num2):
-    for itr in range(0,num2):
+    for itr in range(1,num2):
         if((num1*itr)%num2) == 1:
             return itr
+        else:
+            continue
 
+# Function to convert Original Message from M to M*
 def message(modValue):
     #obj= elGamal()
     print("Please Enter the vale for M as an integer")
@@ -103,40 +106,56 @@ def message(modValue):
     m+=1
 
     mStar = pow(m,2)% modValue
+    print("The vaue for m* is: ")
     print(mStar)
     return mStar
-        
+
+# Function to choose y at random       
 def yGenerator(q):#
     y = random.choice(EulerGroup(q)) # To get a random y from Z*q
+    print("Y chosen is: "+str(y))
     return y
 
+# Function to choose randome Key x  
 def privateKey(q):
     x = random.choice(EulerGroup(q)) # To get a random x from Z*q
+    print("X chosen is: ",str(x))
     return x
 
+# Function to choose g 
 def gGenerator(p):
     g = random.choice(numInGroup(p)) # To get a random g from G(p).
+    print("The g chosen is: "+str(g))
     return g
 # Function to generate a Public Key <G,q,g,h>
+
+# Function to generate h
 def publicKey(p,q,power,g): # generating Public Key
-    #g = gGenerator(p) # To get a random g from G(p).
-    #print("value of g in pubkey is: "+ str(g))
-    #power = privateKey(q) # Get the private key x to calculate H
     # calcuate h
     h = modExponent(g,power,p)
+    print("The value of h is: "+str(h))
     return h 
 
 # Function to Generate Cipher Text 1
 def cipherText1(g,y,p):
+    # Cipher Text 1 is g**y mod p
     CT1 = modExponent(g,y,p)
+
     #print("CipherText 1 is "+str(CT1))
     return CT1
+
+
 # Function to Generate Cipher Text 2
 def cipherText2(h,y,m,p):
+    # Cipher Text 2 is (h**y *mStar)mod p
+    # I have split the Cipher Text Calculations into two parts
+    # Part 1 is calcuting h**y mod p
     Pt1= modExponent(h,y,p)
-    #print(Pt1)
+    
+    #Part 2 is mstar % p
     Pt2 = m % p
-    #print(Pt2)
+    
+    # Finally I calculate  the entire thing properly
     CT2 = (Pt1*Pt2)%p
     #CT2 = modExponent(Pt1*Pt2,1,p)
     #print("CipherText 2 is "+ str(CT2)) 
@@ -144,44 +163,43 @@ def cipherText2(h,y,m,p):
 
 # Function to encrypt the message and display ciphertexts
 def encryption(g,y,h,p,m,x):
+
     print("Genertating Cipher Text 1")
+    #Calculate C1
     c1= cipherText1(g,y,p)
-    #print("Cipher Text 1 is: "+ str(c1))
-    #m=message(p)
-    #print("Genertating Cipher Text 2")
+    print("Cipher Text 1 is: "+str(c1))
+
+    # Calculate C2
     c2 = cipherText2(h,y,m,p)
+
     print("Cipher Text 2 is: "+ str(c2))
+
     print("Decrypting the Message")
     decryption(c1,c2,x,p)
 
 
 # Function to Decrypt the cipher texts to give us 
 def decryption(c1,c2,x,p):
-    # Calculating C1^x mod p
-    C1X = modExponent(c1,x,p)
-    #C1X * value = 1 mod p
-    invC1X = multInverse(C1X,p)
-    pd = c2*invC1X
-    orgMstar = pd% p
-    print("Original M* is: "+ str(orgMstar))
-    #orgMessage= 0
-    result = 0
-    # a^2 mod p = m*
-    # a = math.sqrt(m* + p*i)
-    for i in range(p):
-        for a in range (p):
 
-            rhs = p*a + orgMstar
-            answer = math.sqrt(rhs)
-            if pow(i,2) == rhs and answer < orgMstar:
-                #print("Answer is:"+ str(answer))
-                result = int(answer)
+    # To decrypt the cipher text to get the original message is 
+    # c2/c1**x = h**y.m/g**xy => g**xy*m/g**xy = m
+    # denom = c1**x mod p
+    # numerator * Inverse(denmo) = decryoted message
+
+    denom = modExponent(c1,x,p)
+    print("C1**x is: "+str(denom))
     
-    orgM = result - 1
+    # Calculating Inverse of Denominator
+    # Inverse of Denominator is denmo * inverse of Denmo = 1 mod p
+    invDenmo  = multInverse(denom,p)
+    print("Inverse of C1**x is: "+str(invDenmo))
+    prod = c2*invDenmo
+    mStar = prod % p
+    print("Hence mStar is: "+str(mStar))
 
-    print("The Original message Encrypted was: " + str(orgM))
-    return orgM
-
+    
+        
+  
 # Main Function which would act as the Driver Code
 def main():
     print("*************************** Simple Program for EL-GAMAL Algorithm(Quadratic residue) ***************************")
@@ -200,6 +218,7 @@ def main():
     x = privateKey(q)
     # calculating h
     h = publicKey(p,q,x,g)
+
     
     encryption(g,y,h,p,m,x)
        
