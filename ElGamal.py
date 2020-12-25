f8fbd5ff89a3e6eb86d14b7cd558df681d4271a1
Ajinkya Mukherjee <aam5802@outlook.com>
2020-12-25 14:06:05 +0000

    commit with Euler

--- ElGamal.py
+++ ElGamal.py
@@ -4,10 +4,66 @@ import keyword
 import random
 from typing import MutableSequence
 
+# Public key is <G,q,g,h>
+# Private Key is <G,q,g,x>
+
+def HCF(num1,num2):
+    if num1<num2:
+        return HCF(num2,num1)
+    elif num1 % num2 == 0:
+        return num2
+    else:
+        return HCF(num2,num1%num2)
+def coprime(num1,num2):
+    return HCF(num1,num2)==1
+
+def EulerGroup(x):
+    phiGroup=[]
+    if x == 1:
+        return 1
+    else:
+        for a in range(1,x):
+            if coprime(x,a):
+                phiGroup.append(a)
+    print (phiGroup)
+    return phiGroup
 
 
-class elGamal():
+def numInGroup(x): # function to define the elements in quadratic group
+    GList=[]
+    for i in range (1,x):
+        res = pow(i,2) % x
+        GList.append(res)
+    GList.sort()
+    #print(GList)
+    newGlist= list((dict.fromkeys(GList))) # To remove duplplicates from the list
+    #print (newGlist)
+    return newGlist
 
+#def publicKey(): # generating Public Key
+
+
+
+def message(modValue):
+    #obj= elGamal()
+    print("Please Enter the vale for M as an integer")
+    m = int(input())
+    m+=1
+
+    mStar = pow(m,2)% modValue
+    print(mStar)
+    return mStar
+        
+   
+    
+def encryption(encValue,encOption):
+        
+    val = random.choice(encOption)
+    print(val)
+
+    
+
+def main():
     print("*************************** Simple Program for EL-GAMAL Algorithm ***************************")
     print("Enter the value for q :")
     q = int(input())
@@ -15,42 +71,15 @@ class elGamal():
     
     p = (2*q) + 1
 
-    print("The value of P is "+ str(p))
-    
-    
-    def numInGroup(x):
-        GList=[]
-        for i in range (1,x):
-            res = pow(i,2) % x
-            GList.append(res)
-        GList.sort()
-        #print(GList)
-        newGlist= list((dict.fromkeys(GList))) # To remove duplplicates from the list
-        #print (newGlist)
-        return newGlist
-
+    print("The value of P is "+ str(p)) 
     encList = numInGroup(p) # To chose a randome y from the list
     print(encList)
-    def message(modValue):
-        #obj= elGamal()
-        print("Please Enter the vale for M as an integer")
-        m = int(input())
-        m+=1
-
-        mStar = pow(m,2)% modValue
-        print(mStar)
-        return mStar
-        
     encVal=message(p)
-    
-    def encryption(encValue,encOption):
-        
-        val = random.choice(encOption)
-        print(val)
-
     print("Lets Encrypt a the message")
     encryption(encVal,encList)
-        
+    
+    if __name__ == '__main__':
+        main()
 
 
 
